1b1c6e320e4bb9347fd1a93ed5be44582f9b9dfe
Efrén Paolo Centeno Rosas <efren.centeno@utec.edu.pe>
2025-05-07 22:54:22 -0500

    Actualización de relaciones en la entidad Company

--- src/main/java/com/example/hackaton20251/Company/domain/Company.java
+++ src/main/java/com/example/hackaton20251/Company/domain/Company.java
@@ -3,16 +3,21 @@ package com.example.hackaton20251.Company.domain;
 import com.example.hackaton20251.CompanyRestriction.domain.CompanyRestriction;
 import com.example.hackaton20251.user.domain.User;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Setter
+@Getter
 @Table(name = "companies")
 public class Company {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
 
     private String name;
 
@@ -24,11 +29,16 @@ public class Company {
 
     private Boolean active;
 
-    @OneToMany(mappedBy = "company", cascade = CascadeType.ALL)
-    private List<CompanyRestriction> restrictions;
+    @OneToOne
+    @JoinColumn(name = "administrador_id")
+    private User administrador;
 
     @OneToMany(mappedBy = "company", cascade = CascadeType.ALL)
-    private List<User> users;
+    private List<User> user = new ArrayList<>();
+
+    @OneToMany(mappedBy = "company", cascade = CascadeType.ALL)
+    private List<CompanyRestriction> restricciones = new ArrayList<>();
+
 
 }
 
